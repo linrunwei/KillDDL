fd16c628b25487bd17bc70a6239fc8241ae444ed
Wanchen Jiang <wanchenj@usc.edu>
2018-10-19 22:19:00 -0700

    Emulator App Launch

--- app/src/main/java/app/killddl/killddl/MainActivity.java
+++ app/src/main/java/app/killddl/killddl/MainActivity.java
@@ -2,6 +2,8 @@ package app.killddl.killddl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    public void SignUp(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+        startActivity(intent);
+    }
+
 }
