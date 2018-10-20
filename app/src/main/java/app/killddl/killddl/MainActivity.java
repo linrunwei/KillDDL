0502ac48ded85923e96730124966d85c2c217606
lofnheart <jinpengh@usc.edu>
2018-10-20 00:45:05 -0700

    change back to nealy commerge

--- app/src/main/java/app/killddl/killddl/MainActivity.java
+++ app/src/main/java/app/killddl/killddl/MainActivity.java
@@ -1,16 +1,31 @@
 package app.killddl.killddl;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.view.View;
 import android.content.Intent;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout loginLayout;
+    AnimationDrawable animationDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        loginLayout = (LinearLayout) findViewById(R.id.loginLayout);
+        animationDrawable = (AnimationDrawable) loginLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(4500);
+        animationDrawable.setExitFadeDuration(4500);
+        animationDrawable.start();
+
+
     }
 
     @Override
