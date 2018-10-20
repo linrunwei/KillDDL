83e39dfac6acadb2dbd37271016baed0dd3db017
lofnheart <jinpengh@usc.edu>
2018-10-20 14:50:23 -0700

    Change of login functionality

--- app/src/main/java/app/killddl/killddl/MainActivity.java
+++ app/src/main/java/app/killddl/killddl/MainActivity.java
@@ -5,7 +5,10 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,11 +21,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //animate background
         loginLayout = (LinearLayout) findViewById(R.id.loginLayout);
         animationDrawable = (AnimationDrawable) loginLayout.getBackground();
         animationDrawable.setEnterFadeDuration(4500);
         animationDrawable.setExitFadeDuration(4500);
         animationDrawable.start();
+
+        //login button
+        //TODO connect to database
+        Button loginBtn = (Button) findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText usernameEditText = (EditText) findViewById(R.id.login_username);
+                EditText passwordEditText = (EditText) findViewById(R.id.login_password);
+                TextView errorMsg = (TextView) findViewById(R.id.errorMsg);
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                System.out.println("username: " + username + " password: " + password); //TODO remove this after connection
+                if(password.length() == 0){errorMsg.setText("Password cannot be empty!");}
+                if(username.length() == 0){errorMsg.setText("Username cannot be empty!");}
+                if(false){errorMsg.setText("Username/Password combination wrong!");} //TODO check database
+            }
+        });
 
 
     }
