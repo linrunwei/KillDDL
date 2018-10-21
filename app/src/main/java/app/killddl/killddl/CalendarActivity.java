50ee04c7e49cc4bde311be71bd506d1eecae973f
lofnheart <jinpengh@usc.edu>
2018-10-21 00:22:03 -0700

    addtask functionality

--- app/src/main/java/app/killddl/killddl/CalendarActivity.java
+++ app/src/main/java/app/killddl/killddl/CalendarActivity.java
@@ -1,5 +1,6 @@
 package app.killddl.killddl;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -10,10 +11,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class CalendarActivity extends AppCompatActivity{
     Dialog myDialog;
+    TextView mDisplayDate;
+    DatePickerDialog.OnDateSetListener mDateSetListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +41,17 @@ public class CalendarActivity extends AppCompatActivity{
                 }
             }
         });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month++;
+                mDisplayDate = (TextView) findViewById(R.id.addtask_date);
+                String date = month + "/" + day + "/" + year;
+                if(mDisplayDate != null) mDisplayDate.setText(date);
+                System.out.println(date);
+            }
+        };
     }
 
         public void ShowPopup(View v) {
@@ -52,6 +69,23 @@ public class CalendarActivity extends AppCompatActivity{
             myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
             myDialog.show();
         }
+
+        public void ShowDate(View v){
+            Calendar cal = Calendar.getInstance();
+            int year = cal.get(Calendar.YEAR);
+            int month = cal.get(Calendar.MONTH);
+            int day = cal.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog dialog = new DatePickerDialog(
+                    CalendarActivity.this,
+                    android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                    mDateSetListener,
+                    year,month,day);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.show();
+        }
+
+
     }
 
 
