dead5634d839784b9f4c64dc6cf71ecbd7143336
Hao Chen <chen579@usc.edu>
2018-10-29 16:46:45 -0700

    junit_test_task

--- app/src/androidTest/java/app/killddl/killddl/profilescreen/ProfileScreenTest.java
+++ app/src/androidTest/java/app/killddl/killddl/profilescreen/ProfileScreenTest.java
@@ -47,8 +47,10 @@ public class ProfileScreenTest {
 
     @Test
     public void clickLogOutButton_LogOut(){
+        try{ Thread.sleep(3000); }catch (Exception _){}
+
         //click on logout button
-        onView(withId(R.id.profile_logoutBtn)).perform(click(), closeSoftKeyboard());
+        onView(withId(R.id.profile_logoutBtn)).perform(click());
 
         try{ Thread.sleep(3000); }catch (Exception _){}
 
