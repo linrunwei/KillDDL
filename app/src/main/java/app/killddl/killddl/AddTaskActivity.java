6290aa70076b9c56eed7d72f3d79c288d883f91a
Runwei Lin <runweili@usc.edu>
2018-10-29 22:55:51 -0700

    update and cancel notification after editting tasks

--- app/src/main/java/app/killddl/killddl/AddTaskActivity.java
+++ app/src/main/java/app/killddl/killddl/AddTaskActivity.java
@@ -42,12 +42,12 @@ public class AddTaskActivity extends AppCompatActivity {
     Boolean dateSet = false;
     Boolean timeSet = false;
 
-
     private int year;
     private int month;
     private int day;
     private int hour;
     private int minute;
+    private int taskId;
 
     private CalendarView mCalendarView;
     CollectionReference mColRef;
@@ -87,8 +87,7 @@ public class AddTaskActivity extends AppCompatActivity {
                 year = myear;
                 month = mmonth;
                 day = mday;
-                mmonth = mmonth + 1;
-
+                mmonth += 1;
                 String date = mmonth + "/" + day + "/" + year;
                 SpannableString content = new SpannableString(date);
                 content.setSpan(new UnderlineSpan(), 0, date.length(), 0);
@@ -101,7 +100,7 @@ public class AddTaskActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Calendar calendar = Calendar.getInstance();
-                int mHour = calendar.get(Calendar.HOUR);
+                int mHour = calendar.get(Calendar.HOUR_OF_DAY);
                 int mMinute = calendar.get(Calendar.MINUTE);
                 TimePickerDialog dialog = new TimePickerDialog(
                         AddTaskActivity.this,
@@ -175,7 +174,8 @@ public class AddTaskActivity extends AppCompatActivity {
             err.setText("Cannot Create task before time!");
             return;
         }
-        Tasks task = new Tasks(MainActivity.getDatabase().getTaskList().size(), timestamp);
+        taskId = MainActivity.getDatabase().getTaskList().size();
+        Tasks task = new Tasks(taskId, timestamp);
         task.EditColor(color);
         task.EditDescription(description);
         task.EditName(taskName);
@@ -205,8 +205,10 @@ public class AddTaskActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), NotificationReceiver.class);
         intent.putExtra("taskName", taskName);
         intent.putExtra("frequency", frequency);
+        intent.putExtra("taskId", taskId);
 
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, new Random().nextInt(2048), intent, PendingIntent.FLAG_UPDATE_CURRENT);
+//        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, new Random().nextInt(2048), intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), taskId, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
         if (alarmManager != null) {
@@ -221,10 +223,8 @@ public class AddTaskActivity extends AppCompatActivity {
                                 + minute + ":"
                                 + second
                         , Toast.LENGTH_LONG).show();
-
             }
             else {
-
                 alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
                 Toast.makeText(this,
                         "scheduled one time notification at "
@@ -236,12 +236,9 @@ public class AddTaskActivity extends AppCompatActivity {
                                 + second
                         , Toast.LENGTH_LONG).show();
             }
-
         }
         else {
             Toast.makeText(this, "set notification failed", Toast.LENGTH_LONG).show();
         }
     }
-
 }
-
