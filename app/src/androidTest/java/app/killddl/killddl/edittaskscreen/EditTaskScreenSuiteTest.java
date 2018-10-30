60bc09bd8933a599e53fa33d580f71fced147880
YanxiLi <yanxili@usc.edu>
2018-10-29 18:42:07 -0700

    black test

---
+++ app/src/androidTest/java/app/killddl/killddl/edittaskscreen/EditTaskScreenSuiteTest.java
@@ -0,0 +1,12 @@
+package app.killddl.killddl.edittaskscreen;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import app.killddl.killddl.mainscreen.LoginSetup;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({LoginSetup.class, EditTaskScreenTest.class
+})
+public class EditTaskScreenSuiteTest {
+}
